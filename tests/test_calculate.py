775ce25804ad7438078579c09152eef6457ae325
Sebastien de Villiers <sdevilliers@bamboohr.com>
2024-12-14 21:48:29 -0700

    remove comments

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -1,6 +1,5 @@
 import pytest
 from src.calculateWorkHoursHelpers import calculate
-# import pdb
 
 def test_calculate_single_day():
 	printInputs = []
@@ -10,8 +9,6 @@ def test_calculate_single_day():
 			"Enter the path to your CSV file: ": "../test-data/single-day.csv"
 		}
 		return switch.get(prompt, "")
-
-	# pdb.set_trace()
 
 	calculate(print, input)
 	expected = ["2024", "August", "Mon 26: 8.92h or 8h 55min", "Mon 26 -> Mon 26: 8.92h or 8h 55min"]
@@ -25,8 +22,6 @@ def test_calculate_multiple_days():
 			"Enter the path to your CSV file: ": "../test-data/multi-day.csv"
 		}
 		return switch.get(prompt, "")
-
-	# pdb.set_trace()
 
 	calculate(print, input)
 	expected = ["2024","August", "Mon 26: 8.92h or 8h 55min", "Tue 27: 9.02h or 9h 1min", "Wed 28: 7.80h or 7h 48min", "Thu 29: 7.60h or 7h 36min", "Mon 26 -> Thu 29: 33.33h or 33h 20min", "September", "Tue  3: 4.69h or 4h 41min", "Tue  3 -> Tue  3: 4.69h or 4h 41min"]
